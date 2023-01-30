16b4844d4b0fb33ea113b764b1c315e8c00a85c0
revyrob <kayle.robson@gmail.com>
2023-01-30 13:28:03 -0800

    change it to me not we

--- src/components/Serv/Serv.jsx
+++ src/components/Serv/Serv.jsx
@@ -5,9 +5,9 @@ function Serv() {
     <section id="servcies" className='max-w-screen-xl mx-auto'>
         <h1 className='text-2xl font-black mt-8 mb-4 '>Services</h1>
         <h2>Shopping and Delivery for your food and drink vacation needs.</h2>
-        <p>Send us your shopping list, arrival date, and time.  We will pick-up your groceries, drinks, and deliver them to your door.</p>
-        <p>We charge an hourly rate along with a service charge on items picked up.</p>        
-        <p>Contact us for a quote.</p>
+        <p>Send me your shopping list, arrival date, and arrival time.  I will pick-up your groceries, drinks, and deliver them to your door.</p>
+        <p>I charge an hourly rate along with a service charge on items picked up.</p>        
+        <p>Contact me for a quote.</p>
     </section>
   )
 }
