d069aed30319e474f2e4f4f81d00e4bf43c34bca
revyrob <kayle.robson@gmail.com>
2023-02-10 15:35:20 -0800

    changed order in banner and fixed about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,7 +7,7 @@ function About() {
        
         <div className='sm:flex flex-col md:flex-row justify-around'>
         <img src={pic} alt="kayle robson" className='rounded-full flex items-center justify-center max-w-[300px]'/>
-        <div className='w-1/2'>
+        <div className='sm:w-full md:w-1/2'>
         <h1 className='text-2xl font-black pt-8 mb-4 text-center md:text-3xl'>About</h1>
         <p className='text-left px-8 pb-8 text-2xl leading-relaxed'>Kayle Robson has been working in the service industry for more than a decade.  She is bilingual and has worked across 
           Western Canada and Europe.  With a passion for food and good service Kayle strives to meet clients needs.</p>    
--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -4,14 +4,19 @@ import rossland from '../../assets/images/rossland.jpg';
 function Banner() {
   return (
     <section className='flex flex-col md:flex-row justify-center items-center mx-auto max-w-screen-xl'>
-<div className='w-full md:w-1/2 flex flex-col justify-between items-center font-black'><h1 className='text-4xl my-4'>Simple</h1>
-<h1 className='text-4xl my-4'>Fast</h1>
-<h1 className='text-4xl my-4'>Friendly</h1></div>
-<div className='w-full md:w-1/2'><img src={rossland} alt='rossland' className='w-full'></img>
-</div>
+
+    <div className='sm:order-1 w-full md:w-1/2 order-2 flex flex-col justify-between items-center font-black'>
+    <h1 className='text-4xl my-4'>Simple</h1>
+    <h1 className='text-4xl my-4'>Fast</h1>
+    <h1 className='text-4xl my-4'>Friendly</h1>
+    </div>
+    
+    <div className='sm:order-2 w-full md:w-1/2 order-1'>
+      <img src={rossland} alt='rossland' className='w-full'/>
+    </div>
     </section>
     
-  )
-}
-
-export default Banner
+    )
+  }
+  
+  export default Banner
