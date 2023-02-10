965d00502d3eed8f7e0c62290efe6e6ad9edd917
revyrob <kayle.robson@gmail.com>
2023-02-10 15:18:34 -0800

    working on website

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,13 +3,13 @@ import pic from '../../assets/images/kayle2.png';
 
 function About() {
   return (
-    <section id="about" className='max-w-screen-xl mx-auto md:w-1/2'>
-      
-        <h1 className='text-2xl font-black mt-8 mb-4 '>About</h1>
-        <div className='sm:flex flex-col md:flex-row justify-between'>
+    <section id="about" className='max-w-screen-xl mx-auto p-8 '>
+       
+        <div className='sm:flex flex-col md:flex-row justify-around'>
         <img src={pic} alt="kayle robson" className='rounded-full flex items-center justify-center max-w-[300px]'/>
         <div className='w-1/2'>
-        <p>Kayle Robson has been working in the service industry for more than a decade.  She is bilingual and has worked across 
+        <h1 className='text-2xl font-black pt-8 mb-4 text-center md:text-3xl'>About</h1>
+        <p className='text-left px-8 pb-8 text-2xl leading-relaxed'>Kayle Robson has been working in the service industry for more than a decade.  She is bilingual and has worked across 
           Western Canada and Europe.  With a passion for food and good service Kayle strives to meet clients needs.</p>    
           </div>
           </div>
--- src/components/Serv/Serv.jsx
+++ src/components/Serv/Serv.jsx
@@ -2,10 +2,15 @@ import React from 'react'
 
 function Serv() {
   return (
-    <section id="servcies" className='max-w-screen-xl mx-auto md:w-1/2'>
-        <h1 className='text-2xl font-black mt-8 mb-4 '>Services</h1>
-        <p>Shopping and delivery service for your food/drink vacation needs.  LeRoi Delivery runs on three basic fundamentals: Simple, Fast, and Friendly.  Charges incur include an hourly rate and a service charge on items picked up.</p>    
-        <p>Contact me for a quote.</p>
+    <section id="servcies" className='max-w-screen-xl mx-auto p-8 bg-[#F3403D] '>
+      <div className='sm:w-full md:w-1/2'>
+        <h1 className='text-2xl font-black pt-8 mb-4 align-left md:text-3xl'>Services</h1>
+        <p className='text-slate-200 text-left px-8 pb-8 text-2xl leading-relaxed'>Shopping and delivery service for your food/drink vacation needs.  LeRoi Delivery runs on three basic fundamentals: Simple, Fast, and Friendly.  Charges incur include an hourly rate and a service charge on items picked up.  
+        Contact me for a quote.</p>
+        </div>
+        <div className='sm:w-full h-0 md:w-1/2'>
+
+        </div>
     </section>
   )
 }
