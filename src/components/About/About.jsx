504e8727e3a56777ef6183bbc94f60cea9e21faa
revyrob <kayle.robson@gmail.com>
2023-02-25 16:47:14 -0800

    fixed width and margin on image within the about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,17 +4,17 @@ import pic from '../../assets/images/kayle2.png';
 function About() {
   return (
     <section id="about" className='max-w-screen-xl mx-auto p-8 '>
-       
-        <div className='sm:flex flex-col md:flex-row justify-around'>
-        <img src={pic} alt="kayle robson" className='rounded-full flex items-center justify-center max-w-[300px]'/>
-        <div className='sm:w-full md:w-1/2'>
-        <h1 className='text-2xl font-black pt-8 mb-4 text-center md:text-3xl'>About</h1>
-        <p className='text-left px-8 pb-8 text-2xl leading-relaxed'>Kayle Robson has been working in the service industry for more than a decade.  She is bilingual and has worked across 
-          Western Canada and Europe.  With a passion for food and good service Kayle strives to meet clients needs.</p>    
-          </div>
-          </div>
+    
+    <div className='sm:flex flex-col md:flex-row justify-around'>
+    <img src={pic} alt="kayle robson" className='rounded-full flex items-center justify-center m-auto'/>
+    <div className='sm:w-full md:w-1/2'>
+    <h1 className='text-2xl font-black pt-8 mb-4 text-center md:text-3xl'>About</h1>
+    <p className='text-left px-8 pb-8 text-2xl leading-relaxed'>Kayle Robson has been working in the service industry for more than a decade.  She is bilingual and has worked across 
+    Western Canada and Europe.  With a passion for food and good service Kayle strives to meet clients needs.</p>    
+    </div>
+    </div>
     </section>
-  )
-}
-
-export default About
+    )
+  }
+  
+  export default About
